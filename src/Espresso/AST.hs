b42e0c6b7aea787c83e693ad492e5c632bed9c80
helino <erik.helin@gmail.com>
2011-06-19 11:55:41 +0200

    added doc for the espresso source code

--- src/Espresso/AST.hs
+++ src/Espresso/AST.hs
@@ -1,4 +1,7 @@
+-- | This module contains the abstract syntax tree for the MiniJava language 
+-- used in the compiler
 module Espresso.AST where
 
+-- | The datatype for an expression in MiniJava
 data MJExpression = Identifier String
                     deriving (Show, Eq)
--- src/Espresso/Parser.hs
+++ src/Espresso/Parser.hs
@@ -1,3 +1,4 @@
+-- | This module contains the parser for the MiniJava language
 module Espresso.Parser(
     Espresso.Parser.parse,
     identifier) where
@@ -7,9 +8,12 @@ import Text.Parser.Combinators
 import Text.Parser.Char
 import Espresso.AST
 
+-- | 'parse' parses a string representing a MiniJava program into an abstract 
+-- syntax tree representing the program or returns an error.
 parse :: String -> Either String MJExpression
 parse s = P.parse identifier s
 
+-- | 'identifier' parses an identifier in the MiniJava language.
 identifier :: Parser MJExpression
 identifier =  do x  <- (char '_' <|> letter)
                  xs <- many (alphaNum <|> char '_')
